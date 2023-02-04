7d8686cd7f66d9fd23909f0fd1c4e9f7c8bf5979
“Bo <2571442z@student.gla.ac.uk>
2023-02-04 01:56:49 +0000

    Chapter 8 exercise

--- rango/forms.py
+++ rango/forms.py
@@ -13,8 +13,7 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH,
-    help_text="Please enter the title of the page.")
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH,help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200,help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
