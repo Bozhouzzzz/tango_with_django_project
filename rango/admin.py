2873f7d9e8018718409cc619740de10f14826469
“Bo <2571442z@student.gla.ac.uk>
2023-02-08 19:38:27 +0000

    chapter 9

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from rango.models import UserProfile
 # Register your models here.
 from django.contrib import admin
 from rango.models import Category, Page
@@ -13,5 +13,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Page, PageAdmin)
 admin.site.register(Category,CategoryAdmin)
-
+admin.site.register(UserProfile)
 
--- rango/forms.py
+++ rango/forms.py
@@ -40,3 +40,17 @@ class PageForm(forms.ModelForm):
         exclude = ('category',)
  # or specify the fields to include (don't include the category field).
  #fields = ('title', 'url', 'views')
+from django.contrib.auth.models import User
+from rango.models import UserProfile
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
